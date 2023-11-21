745e38e2477ead4355e320ef032b9a81d09590bf
Pluto <33441379+Linhkust@users.noreply.github.com>
2023-11-21 10:00:50 +0800

    Update client.py

--- main/pipeline generation/client.py
+++ main/pipeline generation/client.py
@@ -24,25 +24,22 @@ server_address = 'Your Server IP Address'
 
 print(f'Connecting to server {server_address}...')
 
-# 注意验证码要保持一致
 m = BaseManager(address=(server_address, 5000), authkey=b'password')
 m.connect()
 
-# 获取queue对象
 task = m.get_task_queue()
 result = m.get_result_queue()
 
-# 开始进行计算
-for i in range(256):
+for i in range(512):
     try:
-        pipeline = task.get(timeout=1)  # 获取manager进程放入task中的值
+        pipeline = task.get(timeout=1)
 
         # computing part
         r = reg.pipelines_fit_single(pipeline)
 
         time.sleep(1)
 
-        result.put(r)  # 将计算的结果放入result队列中
+        result.put(r)
 
     except queue.Empty:
         print('task queue is empty.')
